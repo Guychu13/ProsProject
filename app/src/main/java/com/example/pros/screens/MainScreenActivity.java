aafd6d28cf298a507fb6188ee3f45f6a98d46d75
Guychu <guygb2000@gmail.com>
2021-03-13 14:17:45 +0200

    Working on the FriendlyGameWaitingRoomActivity, the insert game code is good

--- app/src/main/java/com/example/pros/screens/MainScreenActivity.java
+++ app/src/main/java/com/example/pros/screens/MainScreenActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -16,7 +17,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.pros.R;
 import com.example.pros.db.MultiPlayerGameDao;
-import com.example.pros.model.User;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -27,9 +27,9 @@ public class MainScreenActivity extends AppCompatActivity {
 
     private Dialog joinGameDialog;
     private EditText gameCodeDialogEditText;
-    private ImageButton joinGameDialogImageButton;
+    private Button joinGameDialogButton;
     private String gameCodeTyped;
-
+    boolean gameCodeExists;
     private MultiPlayerGameDao multiPlayerGameDao;
 
     private TextView userGreet;
@@ -114,32 +114,31 @@ public class MainScreenActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void openJoinGameDialog(View view) {
+    public void goToJoinGameDialog(View view) {
         joinGameDialog = new Dialog(MainScreenActivity.this);
-        joinGameDialog.setContentView(R.layout.dialog_join_offline_game);
+        joinGameDialog.setContentView(R.layout.dialog_join_online_game);
         joinGameDialog.setCanceledOnTouchOutside(true);
         joinGameDialog.setCancelable(true);
         joinGameDialog.show();
 
         gameCodeDialogEditText = joinGameDialog.findViewById(R.id.editText_joinGameDialog_codeEditText);
-        joinGameDialogImageButton = joinGameDialog.findViewById(R.id.imageButton_mainScreen_joinButton);
+        joinGameDialogButton = joinGameDialog.findViewById(R.id.button_joinGameDialog_joinButton);
 
-        joinGameDialogImageButton.setOnClickListener(new View.OnClickListener() {
+        joinGameDialogButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 gameCodeTyped = gameCodeDialogEditText.getText().toString();
+                gameCodeExists = checkIfGameCodeExists(gameCodeTyped);
+                if(!gameCodeExists){
+                    Toast.makeText(MainScreenActivity.this, "Game code does not exist", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Intent intent = new Intent(MainScreenActivity.this, FriendlyGameWaitingRoomActivity.class);
+                    intent.putExtra("isHost", false);
+                    startActivity(intent);
+                }
             }
         });
-
-        boolean gameCodeExists = checkIfGameCodeExists(gameCodeTyped);
-        if(!gameCodeExists){
-            Toast.makeText(MainScreenActivity.this, "Game code does not exist", Toast.LENGTH_LONG).show();
-        }
-        else{
-            Intent intent = new Intent(MainScreenActivity.this, FriendlyGameWaitingRoomActivity.class);
-            intent.putExtra("isHost", false);
-            startActivity(intent);
-        }
     }
 
     public boolean checkIfGameCodeExists(String gameCode){
