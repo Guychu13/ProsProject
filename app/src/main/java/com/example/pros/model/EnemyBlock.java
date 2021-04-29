23195901d10d728e5d5461fed3f240bbd88324ea
Guychu <guygb2000@gmail.com>
2021-04-29 18:59:44 +0300

    When start is pressed, the game starts on both devices. Good comunication with firebase about the X pos of the blocks. Still not responsive and smooth and still isnt the same gameplay on both devices

--- app/src/main/java/com/example/pros/model/EnemyBlock.java
+++ app/src/main/java/com/example/pros/model/EnemyBlock.java
@@ -14,23 +14,6 @@ public abstract class EnemyBlock extends GameObject {
         this.xSpeed = 12;
     }
 
-//    public void move(int xTarget) {
-//        if (xPos + bitmap.getWidth() / 2 < xTarget) {
-//            for (int i = 0; i < Math.abs(xSpeed); i++) {//כדי שאם יש לחיצה ארוכה הוא יזוז בצעד צעד עד שהוא לא יכול יותר. אם זה זז 20 20 אז הוא לא יסכים לזוז אם הוא נגיד רחוק 18 מהמסגרת
-//                if (xPos + bitmap.getWidth() < windowWidth) {
-//                    xPos += 1;
-//                }
-//            }
-//        }
-//        if (xPos + bitmap.getWidth() / 2 > xTarget) {
-//            for (int i = 0; i < Math.abs(xSpeed); i++) {//כדי שאם יש לחיצה ארוכה הוא יזוז בצעד צעד עד שהוא לא יכול יותר. אם זה זז 20 20 אז הוא לא יסכים לזוז אם הוא נגיד רחוק 18 מהמסגרת
-//                if (xPos > 0) {
-//                    xPos -= 1;
-//                }
-//            }
-//        }
-//    }
-
     public void move(){
         if (xPos + bitmap.getWidth() / 2 < xTarget) {
             for (int i = 0; i < Math.abs(xSpeed); i++) {//כדי שאם יש לחיצה ארוכה הוא יזוז בצעד צעד עד שהוא לא יכול יותר. אם זה זז 20 20 אז הוא לא יסכים לזוז אם הוא נגיד רחוק 18 מהמסגרת
